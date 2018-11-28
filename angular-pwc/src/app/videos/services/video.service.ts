007a68120f80746f5dc26f5312ee53a3fa6cba0c
Dariusz Liszewski <d.liszewski@poczta.onet.pl>
2018-11-28 09:16:31 +0100

    add favourite fix

--- angular-pwc/src/app/videos/services/video.service.ts
+++ angular-pwc/src/app/videos/services/video.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { BASE_URL, IMAGES_BASE_URL } from 'src/app/shared/tokens';
 import { HttpClient } from '@angular/common/http';
 import { Video, FavouriteVideo } from '../model/video.model';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, of, empty } from 'rxjs';
 import { tap, switchMap, mapTo } from 'rxjs/operators';
 
 @Injectable({
@@ -16,13 +16,22 @@ export class VideoService {
    private client: HttpClient) { }
 
    addFavourite(video: Video): Observable<FavouriteVideo> {
-     return this.client.post<FavouriteVideo>(this.base + '/videos', {
-        id: video.id.videoId,
-        video
-     }).pipe(
-       switchMap((favouriteVideo) => {
-        return this.loadFavourite().pipe(mapTo(favouriteVideo));
-       })
+     return of(1).pipe(
+       switchMap((v) => this.loadFavourite()),
+       switchMap(favorites  => {
+         console.log('favs ', favorites.find(v => v.id === video.id.videoId));
+         if (favorites.find(v => v.id === video.id.videoId)) {
+            return empty();
+         } else {
+          return this.client.post<FavouriteVideo>(this.base + '/videos', {
+            id: video.id.videoId,
+            video
+         }).pipe(
+           switchMap((favouriteVideo) => {
+            return this.loadFavourite().pipe(mapTo(favouriteVideo));
+           })
+         );
+         }})
      );
    }
 
