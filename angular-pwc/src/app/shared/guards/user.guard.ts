63d6746935232a7838651bff571ad9ef307e1db2
Dariusz Liszewski <d.liszewski@poczta.onet.pl>
2018-11-29 14:38:55 +0100

    guard change from userService to authService

--- angular-pwc/src/app/shared/guards/user.guard.ts
+++ angular-pwc/src/app/shared/guards/user.guard.ts
@@ -3,19 +3,21 @@ import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '
 import { Observable } from 'rxjs';
 import {UserService} from '../services/user.service';
 import {map} from 'rxjs/operators';
+import {AuthService} from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(private router: Router, private userService: UserService, private authService: AuthService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     console.log('GUARD ROLES', next.data);
 
-    return this.userService.user$.pipe(
+    // return this.userService.user$.pipe(
+    return this.authService.loginDialog$.pipe(
       map(user => {
         if (next.data.roles && user && next.data.roles.includes(user.role)) {
           return true;
