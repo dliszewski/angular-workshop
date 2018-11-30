3c2d4b7bc2345b70adf3f3dc6bb0e612c1ea2505
Dariusz Liszewski <d.liszewski@poczta.onet.pl>
2018-11-30 15:56:37 +0100

    redux selectors

--- angular-pwc/src/app/store/reducers/videos.reducer.ts
+++ angular-pwc/src/app/store/reducers/videos.reducer.ts
@@ -1,19 +1,22 @@
 import {VideosActions, VideosActionTypes} from '../actions/videos.actions';
 import {User} from '../../videos/model/music.model';
 import {Video} from '../../videos/model/video.model';
+import {createSelector} from '@ngrx/store';
 
 export interface State {
   credits: string;
   user: User | null;
   videos: Video[];
   query: string;
+  selectedVideoId: string;
 }
 
 export const initialState: State = {
   credits: 'Szkolenie Angular 2018',
   user: null,
   videos: [],
-  query: null
+  query: null,
+  selectedVideoId: null
 };
 
 export function reducer(state = initialState, action: VideosActions): State {
@@ -53,3 +56,10 @@ export function reducer(state = initialState, action: VideosActions): State {
       return state;
   }
 }
+
+export const getVideos = (state: State) => state.videos;
+export const getSelectedVideoId = (state: State) => state.selectedVideoId;
+
+export const getSelectedVideo = createSelector(getVideos, getSelectedVideoId, (videos, selectedVideoId) => {
+  return  videos.find(v => v.id.videoId === selectedVideoId);
+});
